3e8df3bb56a2458127d62ee60ffeae1c26d56904
Saikumar0701 <doddapudisaikumar@gmail.com>
2024-01-10 08:41:02 +0530

    Day 1/100 daysOfChallenge

---
+++ BASIC/src/Operator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Operator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=12;
+        int b=13;
+        System.out.println(a-b);
+    }
+}
